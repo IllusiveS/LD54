a336806664060d928181ab9a417d8def8acd6de7
Patryk Wysocki <wysockipawelpatryk@gmail.com>
2023-10-01 12:47:04 +0200

    initital commit

---
+++ Scripts/Sync/PlayerInput.gd
@@ -0,0 +1,37 @@
+# player_input.gd
+extends MultiplayerSynchronizer
+
+# Set via RPC to simulate is_action_just_pressed.
+@export var jumping := false
+
+
+@export var input_dir : Vector2
+@export var direction : Vector3
+@export var rot_speed : float
+
+func _ready():
+	# Only process for the local player.
+	set_process(get_multiplayer_authority() == multiplayer.get_unique_id())
+	set_process_input(get_multiplayer_authority() == multiplayer.get_unique_id())
+	
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+@rpc("any_peer", "call_local", "reliable")
+func fire():
+	get_parent().fire()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		get_parent().target_turret_rot = get_parent().turret.global_basis.rotated(Vector3(0, 1, 0), -event.relative.x * get_process_delta_time())
+		get_parent().target_barrel_rot = get_parent().cannon_pivot.basis.rotated(Vector3(0, 0, 1), event.relative.y * get_process_delta_time())
+
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		fire.rpc()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	input_dir = Input.get_vector("left", "right", "forward", "backward")
+	
+	direction = (get_parent().global_transform.basis * Vector3(0, 0, input_dir.y)).normalized()
+	rot_speed = -input_dir.x * get_parent().SPEED * delta
---
+++ Scripts/tank.gd
@@ -0,0 +1,75 @@
+extends CharacterBody3D
+
+@export var bullet_scene : PackedScene
+@onready var bullet_spawn_point = %BulletSpawn
+
+@onready var turret = $RootNode/Tank/Turret
+@onready var cannon_pivot = $RootNode/Tank/Turret/CannonPivot
+
+@onready var InputSync = $PlayerInput
+
+const SPEED = 15.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
+	update_cam.call_deferred()
+
+func update_cam():
+	if player == multiplayer.get_unique_id():
+		$RootNode/Tank/Turret/Camera3D.current = true
+
+# Set by the authority, synchronized on spawn.
+@export var player := 1 :
+	set(id):
+		player = id
+		# Give authority over the player input to the appropriate peer.
+		$PlayerInput.set_multiplayer_authority(id)
+
+# Player synchronized input.
+@onready var input = $PlayerInput
+
+func fire():
+	var new_bullet : RigidBody3D = bullet_scene.instantiate()
+	get_node("/root/Multiplayer/Level/GameLevel/Bullets").add_child(new_bullet, true)
+	new_bullet.global_transform = bullet_spawn_point.global_transform
+	new_bullet.apply_impulse(new_bullet.global_transform.basis.z * 400)
+
+@onready var target_turret_rot = turret.global_basis
+@onready var target_barrel_rot = cannon_pivot.basis
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = InputSync.input_dir
+	
+	var direction = InputSync.direction
+	var rot_speed = InputSync.rot_speed
+	
+	if direction:
+		#rot_speed = -input_dir.x * SPEED * delta
+		velocity = -direction * SPEED
+	else:
+		#rot_speed = move_toward(rot_speed, 0, SPEED)
+		velocity = lerp(Vector3(), velocity, SPEED * delta)
+	
+	turret.global_basis = lerp(turret.global_basis.get_rotation_quaternion(), target_turret_rot.get_rotation_quaternion(), 5 * delta)
+	
+	cannon_pivot.basis = lerp(cannon_pivot.basis.get_rotation_quaternion(), target_barrel_rot.get_rotation_quaternion(), 5 * delta)
+	
+	var clamped_rot = clamp(cannon_pivot.rotation.z, deg_to_rad(-20), deg_to_rad(20))
+	cannon_pivot.rotation.z = clamped_rot
+	
+	rotate_y(rot_speed * delta * SPEED)
+
+	move_and_slide()
